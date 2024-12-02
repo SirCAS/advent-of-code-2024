4f3c53ffb03067c8a05e8d0a5911328dfc9f802b
Christoffer Illum Søndergaard <sircas@sircas.dk>
2024-12-02 23:22:37 +0100

    feat: day1 part 2 solved

--- day01/main.go
+++ day01/main.go
@@ -32,10 +32,22 @@ func main() {
 	slices.Sort(col1)
 
 	totalDistance := 0.0
+	similarityScore := 0
 
-	for idx := 0; idx < len(lines); idx++ {
-		totalDistance += math.Abs(float64(col0[idx]) - float64(col1[idx]))
+	for idxCol0 := 0; idxCol0 < len(lines); idxCol0++ {
+		totalDistance += math.Abs(float64(col0[idxCol0]) - float64(col1[idxCol0]))
+
+		occur := 0
+		for idxCol1 := 0; idxCol1 < len(lines); idxCol1++ {
+			if col0[idxCol0] == col1[idxCol1] {
+				occur++
+			}
+		}
+
+		similarityScore += col0[idxCol0] * occur
+
 	}
 
-	fmt.Printf("Distance is %0.f", totalDistance)
+	fmt.Printf("  Total distance is : %0.f\n", totalDistance)
+	fmt.Printf("Similarity score is : %d\n", similarityScore)
 }
