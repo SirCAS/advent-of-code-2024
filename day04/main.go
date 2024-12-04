f738d52d8b1fcaf991e7278cf5f8e3d3b02148be
Christoffer Illum Søndergaard <sircas@sircas.dk>
2024-12-04 08:04:03 +0100

    feat: ✨️ add day 4 part 2 solution

--- day04/main.go
+++ day04/main.go
@@ -12,9 +12,11 @@ func main() {
 
 	data := strings.Split(string(input), "\n")
 
-	occurrences := findXmas(data)
+	xmasOccurrences := findXmas(data)
+	fmt.Printf("There is %d xmas'es\n", xmasOccurrences)
 
-	fmt.Printf("there is %d\n", occurrences)
+	crossMasOccurrences := findCrossMas(data)
+	fmt.Printf("There is %d cross-mas'es\n", crossMasOccurrences)
 }
 
 func findXmas(data []string) int {
@@ -56,6 +58,31 @@ func findXmas(data []string) int {
 					occurrences++
 				}
 			}
+		}
+	}
+
+	return occurrences
+}
+
+func findCrossMas(data []string) int {
+	occurrences := 0
+
+	width := len(data[0])
+	height := len(data)
+
+	for y := 0; y < height-2; y++ {
+		for x := 0; x < width-2; x++ {
+
+			diagonal1 := data[y][x] == 'M' && data[y+1][x+1] == 'A' && data[y+2][x+2] == 'S'
+			diagonal1_reverse := data[y][x] == 'S' && data[y+1][x+1] == 'A' && data[y+2][x+2] == 'M'
+
+			diagonal2 := data[y][x+2] == 'M' && data[y+1][x+1] == 'A' && data[y+2][x] == 'S'
+			diagonal2_reverse := data[y][x+2] == 'S' && data[y+1][x+1] == 'A' && data[y+2][x] == 'M'
+
+			if (diagonal1 || diagonal1_reverse) && (diagonal2 || diagonal2_reverse) {
+				occurrences++
+			}
+
 		}
 	}
 
--- day04/main_test.go
+++ day04/main_test.go
@@ -18,9 +18,24 @@ var findXmasTests = []findXmasTest{
 	{[]string{".X....", "..M...", "...A..", "....S.", "......", "......"}, 1},
 }
 
+var findCrossMasTests = []findXmasTest{
+	{[]string{".....", ".M.S.", "..A..", ".M.S.", "....."}, 1},
+	{[]string{".....", ".S.M.", "..A..", ".S.M.", "....."}, 1},
+	{[]string{".....", ".M.M.", "..A..", ".S.S.", "....."}, 1},
+	{[]string{".....", ".S.S.", "..A..", ".M.M.", "....."}, 1},
+}
+
 func TestFindXmas(t *testing.T) {
 	for _, test := range findXmasTests {
 		if output := findXmas(test.arg1); output != test.expected {
+			t.Fatalf("Input %v has count %d and expected %d", test.arg1, output, test.expected)
+		}
+	}
+}
+
+func TestFindCrossMas(t *testing.T) {
+	for _, test := range findCrossMasTests {
+		if output := findCrossMas(test.arg1); output != test.expected {
 			t.Fatalf("Input %v has count %d and expected %d", test.arg1, output, test.expected)
 		}
 	}
