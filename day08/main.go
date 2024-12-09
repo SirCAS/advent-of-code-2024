3212e88341223603d5dd2b0112912fe1ba0424dd
Christoffer Illum Søndergaard <sircas@sircas.dk>
2024-12-09 23:11:29 +0100

    feat: ✨️ add solution for day 8 part 2

--- day08/main.go
+++ day08/main.go
@@ -20,42 +20,82 @@ func main() {
 	}
 	lines := strings.Split(string(input), "\n")
 
-	width, height := len(lines[0]), len(lines)
+	canvas := len(lines)
 
-	antennaGroup := map[rune][]coordinate{}
-	for y := 0; y < height; y++ {
-		for x := 0; x < width; x++ {
+	groups := locateAntennasGroupedByFrequency(lines, canvas)
+
+	antinodes := locateAntinodes(groups, canvas)
+	fmt.Printf("There is %d antinodes\n", len(antinodes))
+
+	antinodesWithHarmonics := locateAntinodesWithHarmonics(groups, canvas)
+	fmt.Printf("There is %d antinodes when considering harmonics too\n", len(antinodesWithHarmonics))
+}
+
+func locateAntennasGroupedByFrequency(lines []string, canvas int) map[rune][]coordinate {
+	groups := map[rune][]coordinate{}
+
+	for y := 0; y < canvas; y++ {
+		for x := 0; x < canvas; x++ {
+
 			field := rune(lines[y][x])
 			if unicode.IsDigit(field) || unicode.IsLetter(field) {
-				antenna := coordinate{
-					x: x,
-					y: y,
-				}
-
-				antennaGroup[field] = append(antennaGroup[field], antenna)
+				groups[field] = append(groups[field], coordinate{x: x, y: y})
 			}
 		}
 	}
 
-	antinode := map[coordinate]bool{}
+	return groups
+}
+
+func locateAntinodes(antennaGroup map[rune][]coordinate, canvas int) map[coordinate]bool {
+	antinodes := map[coordinate]bool{}
 
 	for _, antennas := range antennaGroup {
 		for i := range antennas {
 			for j := range antennas {
+				// Skip pair with own location
 				if i != j {
-
 					coordinate := coordinate{
-						x: antennas[i].x + int(antennas[i].x-antennas[j].x),
-						y: antennas[i].y + int(antennas[i].y-antennas[j].y),
+						x: antennas[i].x + (antennas[i].x - antennas[j].x),
+						y: antennas[i].y + (antennas[i].y - antennas[j].y),
 					}
 
-					if 0 <= coordinate.x && coordinate.x < width && 0 <= coordinate.y && coordinate.y < height {
-						antinode[coordinate] = true
+					// Add to results
+					if isWithinCanvas(coordinate, canvas) {
+						antinodes[coordinate] = true
 					}
 				}
 			}
 		}
 	}
 
-	fmt.Printf("There is %d", len(antinode))
+	return antinodes
+}
+
+func locateAntinodesWithHarmonics(antennaGroup map[rune][]coordinate, canvas int) map[coordinate]bool {
+	antinodes := map[coordinate]bool{}
+	for _, antennas := range antennaGroup {
+		for i := range antennas {
+			for j := range antennas {
+				// "Worst case" would be every possible position from left top corner to bottom
+				// right corner (we could use pyhagotas but for the sake of simplicity assume 2x)
+				for a := 1; a < canvas*2; a++ {
+					coordinate := coordinate{
+						x: antennas[i].x + (antennas[i].x-antennas[j].x)*a,
+						y: antennas[i].y + (antennas[i].y-antennas[j].y)*a,
+					}
+
+					if isWithinCanvas(coordinate, canvas) {
+						antinodes[coordinate] = true
+					}
+				}
+			}
+		}
+	}
+
+	return antinodes
+}
+
+func isWithinCanvas(position coordinate, canvas int) bool {
+	return 0 <= position.x && position.x < canvas && 0 <= position.y && position.y < canvas
 }
